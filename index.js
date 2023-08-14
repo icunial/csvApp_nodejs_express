2cff6e6a4af2be4a52c6e751a0109d95b3cb9c17
Ignacio Cunial <igsecu@hotmail.com>
2023-08-14 10:42:09 -0300

    Created GET route -> get all todos

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// GET all todos
+app.get("/", (req, res, next) => {
+  axios
+    .get("https://jsonplaceholder.typicode.com/todos")
+    .then((results) => {
+      res.status(200).json({
+        statusCode: 200,
+        data: results.data,
+      });
+    })
+    .catch((err) => {
+      return next();
+    });
+});
+
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}...`);
 });
